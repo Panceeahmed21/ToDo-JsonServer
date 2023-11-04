0c2f411204f2254c1ec4e5e1a8c3810727ee3bcb
Pancee Ahmed <Panceeahmed22@gmail.com>
2023-11-04 22:52:10 +0200

    add functionality

--- src/Todolist/list.jsx
+++ src/Todolist/list.jsx
@@ -1,38 +1,116 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Axios } from "./Axios";
+import "../App.css";
 
 const Todolist = () => {
   const [todos, setTodos] = useState([]);
+  const [taskName, setTaskName] = useState("");
+  const [currentAction, setCurrentAction] = useState("add");
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef();
 
-  const handleChange = (e) => {};
+  const searchFn = (e) => {
+    e.preventDefault();
+    setSearchTerm(inputRef.current.value.trim());
+  };
 
-  const handleDelete = (id) => {};
-  const handleEdit = (content) => {};
-  const handleDone = (status) => {};
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      Axios.get(`/todos/?q=${searchTerm}`).then((res) => {
+        setTodos(res.data);
+      });
+    }, 2000);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
+
+  const handleChange = (e) => {
+    setTaskName(e.target.value);
+  };
+  const handleEdit = (todo) => {
+    setEditingTodoId(todo.id);
+    setTaskName(todo.taskName);
+  };
+
+  const handleDelete = (id) => {
+    Axios.delete(`/todos/${id}`).then(() => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    });
+  };
 
   const addTask = async (e) => {
     e.preventDefault();
+
+    if (editingTodoId) {
+      Axios.patch(`/todos/${editingTodoId}`, { taskName }).then((response) => {
+        if (response.status === 200) {
+          setEditingTodoId(null);
+          setTaskName("");
+          todolist();
+        }
+      });
+    } else {
+      server
+        .post("/todos", {
+          taskName,
+          isCompleted: false,
+        })
+        .then((response) => {
+          if (response.status === 201) {
+            setTaskName("");
+            todolist();
+          }
+        });
+    }
   };
+  const handleDone = (todo) => {
+    Axios.put(`/todos/${todo.id}`, { ...todo, isCompleted: true }).then(() => {
+      setTodos((prevTodos) =>
+        prevTodos.map((item) =>
+          item.id === todo.id ? { ...item, isCompleted: true } : item
+        )
+      );
+    });
+  };
+  const todolist = () => {
+    Axios.get("/todos").then((data) => {
+      setTodos(data.data);
+    });
+  };
+  useEffect(() => {
+    todolist();
+  }, []);
 
   return (
     <div className="todolist">
-      <div className="search" onSubmit={addTask}>
-        <input type="text" placeholder="Search ex: todo 1" />
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search ex: todo 1"
+          ref={inputRef}
+          onChange={searchFn}
+        />
       </div>
       <form className="addTask" onSubmit={addTask}>
         <input
+          id="taskInput"
           type="text"
+          value={taskName}
           onChange={handleChange}
           placeholder="Add a task........"
         />
-        <button className="addtask-btn">Add Task</button>
+        <button className="addtask-btn" type="submit">
+          {editingTodoId ? "Update Task" : "Add Task"}
+        </button>
       </form>
       <div className="lists">
-        {todos?.map((todo, id) => (
+        {todos?.map((todo) => (
           <div
-            key={id}
+            key={todo.id}
             className={`list ${todo.isCompleted ? "completed" : ""}`}
           >
-            <p> {todo.taskName}</p>
+            <p>{todo.taskName}</p>
             <div className="span-btns">
               {!todo.isCompleted && (
                 <span onClick={() => handleDone(todo)} title="completed">
